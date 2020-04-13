4721b0d51c21c38d2ced02b85fe0a5b939c0f2f2
Reut Keyshawn <57151614+reutKeyshawn@users.noreply.github.com>
2020-04-13 19:23:51 +0300

    Add files via upload

---
+++ FamilyTree.cpp
@@ -0,0 +1,48 @@
+#include "FamilyTree.hpp"
+#include <iostream>
+#include <string>
+#include <exception>
+using namespace Family;
+using namespace std;
+
+Person::Person(string name, Gender gender)
+{
+    this->name = name;
+    this->father = nullptr;
+    this->mother = nullptr;
+    this->gender = gender;
+}
+
+Tree::Tree(string name)
+{
+
+    this->root = new Person(name);
+}
+
+Tree Tree::addFather(string son_name, string father_name)
+{
+
+    return *this;
+}
+
+Tree Tree::addMother(string son_name, string mother_name)
+{
+
+    return *this;
+}
+
+string Tree::relation(string name){
+    return "relation"}
+
+string Tree::find(string relation)
+{
+    return "name";
+}
+
+void Tree::remove(string name)
+{
+}
+
+void Tree::display()
+{
+}
